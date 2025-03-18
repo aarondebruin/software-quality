e347e36d181d2376d4441f77ec52c98f292ea4b0
Max Appeldorn <maxappeldorn78@gmail.com>
2025-03-18 13:36:36 +0100

    Added maven

--- src/JabberPoint.java
+++ src/JabberPoint.java
@@ -23,7 +23,7 @@ public class JabberPoint {
 
 	/** Het Main Programma */
 	public static void main(String argv[]) {
-		
+
 		Style.createStyles();
 		Presentation presentation = new Presentation();
 		new SlideViewerFrame(JABVERSION, presentation);
