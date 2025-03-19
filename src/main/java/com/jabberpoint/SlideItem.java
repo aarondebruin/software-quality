1bedc4c74c7b5ce370c7d863dc1488023b8a3c00
Max Appeldorn <maxappeldorn78@gmail.com>
2025-03-19 13:12:01 +0100

    Updated check format

--- src/main/java/com/jabberpoint/SlideItem.java
+++ src/main/java/com/jabberpoint/SlideItem.java
@@ -1,11 +1,16 @@
 package com.jabberpoint;
 
-import java.awt.Rectangle;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.image.ImageObserver;
 
-/** <p>De abstracte klasse voor een item op een Slide<p>
- * <p>Alle SlideItems hebben tekenfunctionaliteit.</p>
+/**
+ * De abstracte klasse voor een item op een Slide
+ *
+ * <p>
+ *
+ * <p>Alle SlideItems hebben tekenfunctionaliteit.
+ *
  * @author Ian F. Darwin, ian@darwinsys.com, Gert Florijn, Sylvia Stuurman
  * @version 1.1 2002/12/17 Gert Florijn
  * @version 1.2 2003/11/19 Sylvia Stuurman
@@ -13,29 +18,28 @@ import java.awt.image.ImageObserver;
  * @version 1.4 2007/07/16 Sylvia Stuurman
  * @version 1.5 2010/03/03 Sylvia Stuurman
  * @version 1.6 2014/05/16 Sylvia Stuurman
-*/
-
+ */
 public abstract class SlideItem {
-	private int level = 0; // het level van het slideitem
+  private int level = 0; // het level van het slideitem
 
-	public SlideItem(int lev) {
-		level = lev;
-	}
+  public SlideItem(int lev) {
+    level = lev;
+  }
 
-	public SlideItem() {
-		this(0);
-	}
+  public SlideItem() {
+    this(0);
+  }
 
-// Geef het level
-	public int getLevel() {
-		return level;
-	}
+  // Geef het level
+  public int getLevel() {
+    return level;
+  }
 
-// Geef de bounding box
-	public abstract Rectangle getBoundingBox(Graphics g, 
-			ImageObserver observer, float scale, Style style);
+  // Geef de bounding box
+  public abstract Rectangle getBoundingBox(
+      Graphics g, ImageObserver observer, float scale, Style style);
 
-// teken het item
-	public abstract void draw(int x, int y, float scale,
-                              Graphics g, Style style, ImageObserver observer);
+  // teken het item
+  public abstract void draw(
+      int x, int y, float scale, Graphics g, Style style, ImageObserver observer);
 }
